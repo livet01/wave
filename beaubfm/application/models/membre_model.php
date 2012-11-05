9ce394c8c4f81ce9f4dacf7d59e39ecef62b0123
MathieuCom <mathieu.c@sfr.fr>
2012-11-05 00:33:29 +0000

    Ajout membre_model :)

---
+++ beaubfm/application/models/membre_model.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * 
+ */
+class Diffuseur_model extends CI_Model {
+	
+	protected $vue = 'membre';
+	
+	function __construct() {
+		parent::__construct();
+	}
+		
+	public function readMembre($mem_nom)
+	{
+		return $this->db->select('mem_id')
+						->from($this->vue)
+						->where('mem_nom',$mem_nom);
+	}
+}
