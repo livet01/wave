faf664ddc196c3bf90fa4ac4a81b05ad67daa64c
livet01 <david.livet@orange.fr>
2012-11-05 03:01:21 +0000

    Initial Import

---
+++ beaubfm/application/models/autocomplete_model.php
@@ -0,0 +1,11 @@
+<?php 
+class Autocomplete_Model extends CI_Model
+{
+    function GetAutocomplete($options = array())
+    {
+	    $this->db->select('art_nom');
+	    $this->db->like('art_nom', $options['keyword'], 'after');
+   		$query = $this->db->get('Artiste');
+		return $query->result();
+    }
+}
