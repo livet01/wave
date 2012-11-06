a8fac835cfccda83cb43f493537764af446611f0
sbouaked <samir.bouaked@gmail.com>
2012-11-06 16:53:01 +0000

    Création MY_Controller

--- beaubfm/application/core/MY_Controller.php
+++ beaubfm/application/core/MY_Controller.php
@@ -1,5 +1,6 @@
-<?php 
-class MY_Controller extends Controller
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller
 {
     public function __construct()
     {
