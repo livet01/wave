5b2ffda2ddcaa6a2ced58760ff344cf62ef223f8
MathieuCom <mathieu.c@sfr.fr>
2012-11-08 20:33:06 +0000

    Début du codage de l'import, ça marche pas !!

--- beaubfm/application/controllers/importerFiche.php
+++ beaubfm/application/controllers/importerFiche.php
@@ -12,7 +12,30 @@ class ImporterFiche extends MY_Controller {
 	public function importerFiche($data = array()) {
 		$this->load->library('layout');
 		$this->layout->views('menu_principal')
-					-> view('importer');
+					-> view('importer',array('error' => ' ' ));
+	}
+	
+	public function envoi(){
+		$config['upload_path'] = './assets/upload/';
+		$config['allowed_types'] = 'csv|tkt|xls|xml|jpg';
+		$config['max_size']	= '10000000';
+		$config['max_width']  = '102400000';
+		$config['max_height']  = '76800000';
+		
+		$this->load->library('upload', $config);
+	
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());			
+			$this->load->library('layout');
+			$this->layout->views('menu_principal')
+					-> view('importer',$error);
+		}	
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());			
+			$this->load->view('upload_success', $data);
+		}
 	}
 }
 ?>
--- beaubfm/application/views/importer.php
+++ beaubfm/application/views/importer.php
@@ -1,15 +1,15 @@
 <div id="cadre2">
 	<div id="cadre_action">
 		<a class="btn-large-action" href="javascript:document.forms[0].reset()"><i class="icon-undo"></i> Annuler </a>
-		<a class="btn-large-action" href="javascript:submitform()"><i class="icon-ok"></i> Importer </a>
+		<a class="btn-large-action" href="javascript:document.forms['importer'].submit();"><i class="icon-ok"></i> Importer </a>
 	</div>
 	<div id="resultat_recherche">
 		<div id="cadre_importation">
 			<div class="cadre_selection">
-	
+				<?php echo $error; ?>
 				<!-- Sélection de fichiers -->
 				<h1>Importation</h1>
-				<form action="importer_fiches.php">
+				<form  method="post" name="importer" id="importer" action="<?php echo site_url("importerFiche/envoi"); ?>" enctype="multipart/form-data">
 					<p>
 						<label class="lab_import" for="fichier">Fichier 1:</label>
 						<input size="60" type="file" name="fichier" id="fichier">
