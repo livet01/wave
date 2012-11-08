a7ca76b439cf243ad2f67172095b16c352262929
LinkSC <h23j28@gmail.com>
2012-11-08 12:56:46 +0000

    Affichage temporaire des erreurs des saisie

--- beaubfm/application/views/ajouter_fiche.php
+++ beaubfm/application/views/ajouter_fiche.php
@@ -1,22 +1,24 @@
 <div id="cadre2">
 	<div id="cadre_action">
-		<a class="btn-large-action" href="javascript:document.forms[0].reset()"><i class="icon-undo"></i> Annuler </a>
-		<a class="btn-large-action" href="javascript: submitform()"><i class="icon-ok"></i> Valider </a>
+		<a class="btn-large-action" href="javascript:document.forms[0].reset();"><i class="icon-undo"></i> Annuler </a>
+		<a class="btn-large-action" href="javascript: document.forms['fiche'].submit();"><i class="icon-ok"></i> Valider </a>
 	</div>
 	<div id="resultat_recherche">
 	<h1>Ajout d'une fiche</h1>
 	<div id="formulaire">
-		<form  method="post" id="fiche" onsubmit="return verifForm(this)" action="envoi">
+		<form  method="post" id="fiche" onsubmit="return verifForm(this)" action="<?php echo site_url("ajoutFiche/envoi"); ?>">
 			<div id="gauche">
 				<p>
 					<label class="labelGauche" for="titre"><i class="icon-music"></i> Titre </label>
 					<input type="text" id="titre" name="titre" title="Titre obligatoire" onblur="verifTitre(this)">
+					<?php echo form_error('titre'); ?>
 				</p>
 				<p>
 					<label class="labelGauche" for="artiste"><i class="icon-group"></i> Artiste</label>
 					<input type="text" id="artiste" name="artiste" title="Artiste obligatoire" onblur="verifArtiste(this)">
 					<input class="check" type="checkbox" id="autoprod" name="autoprod" value="a">
 					<label class="labelCheck" for="autoprod">Auto-Production</label>
+					<?php echo form_error('artiste'); ?>
 				</p>
 				<p>
 					<label class="labelGauche" for="format"><i class="icon-file"></i> Format</label>
@@ -45,22 +47,27 @@
 				<p>
 					<label class="labelGauche" for="emBev"><i class="icon-comments"></i> Emission Bénévole </label>
 					<input type="text" name="emBev" id="emBev" onblur="verifEmBen(this)" >
+					<?php echo form_error('emBev'); ?>
 				</p>
 				<p>
 					<label class="labelGauche" for="listenBy"><i class="icon-headphones"></i> Ecouté par </label>
 					<input type="text" name="listenBy" id="listenBy" onblur="verifDiffuseur(this)" >
+					<?php echo form_error('listenBy'); ?>
 				</p>
 				<p>
 					<label class="labelGauche" for="diffuseur"><i class="icon-home"></i> Diffuseur </label>
 					<input type="text" id="diffuseur" name="diffuseur" title="Label obligatoire" onblur="verifDiffuseur(this)">
+					<?php echo form_error('diffuseur'); ?>
 				</p>
 				<p>
 					<label class="labelGauche" for="email"><i class="icon-envelope-alt"></i> Email de contact </label>
 					<input type="text" id="email" name="email" title="e-mail obligatoire" onblur="verifMail(this)">
 					<input class="check" type="checkbox" id="envoiMail" name="envoiMail" value="0">
 					<label class="labelCheck" for="envoiMail" >Envoyer email</label>
+					<?php echo form_error('email'); ?>
 				</p>
 			</div>
-	</div>
+		</form>
+		</div>
 	</div>
 </div>
