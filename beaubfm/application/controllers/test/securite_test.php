a619b58d879ee9b46eccbc8761fc1a728889f77b
livet01 <david.livet@orange.fr>
2012-11-09 20:10:13 +0000

    Ajout de test pour la library securite

---
+++ beaubfm/application/controllers/test/securite_test.php
@@ -0,0 +1,44 @@
+<?php
+require_once('Toast.php');
+
+class Securite_test extends Toast
+{
+	function __construct()
+	{
+		parent::Toast(__FILE__); // Remember this
+		$this->load->library('securite');
+	}
+
+	function test_crypt_difference()
+	{
+		// Test code goes here
+		$mdp = "lapin";
+		$this->_assert_false($this->securite->crypt($mdp)===$mdp);
+		$this->message = "Mdp crypte different du mdp initial ";
+	}
+	
+	function test_crypt_null()
+	{
+		// Test code goes here
+		$mdp = NULL;
+		$this->_assert_empty($this->securite->crypt($mdp));
+		$this->message = "Mdp initial null ";
+	}
+	
+	function test_crypt_vide()
+	{
+		// Test code goes here
+		$mdp = "";
+		$this->_assert_not_empty($this->securite->crypt($mdp));
+		$this->message = "Mdp initial vide ";
+	}
+	
+	function test_crypt_egalite()
+	{
+		// Test code goes here
+		$mdp = "AZERTYUIOPQSDFGHJKLMWXCVBN";
+		$mdp2 = "AZERTYUIOPQSDFGHJKLMWXCVBN";
+		$this->_assert_equals($this->securite->crypt($mdp),$this->securite->crypt($mdp2));
+		$this->message = "Deux mots de passe egals.";
+	}
+}
