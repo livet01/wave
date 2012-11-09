048ddba0d2342f5c1ab61fd301ee8d913a64b67b
MathieuCom <mathieu.c@sfr.fr>
2012-11-09 09:09:46 +0000

    Import et import multiple marchent

--- beaubfm/application/controllers/importerFiche.php
+++ beaubfm/application/controllers/importerFiche.php
@@ -2,40 +2,43 @@
 class ImporterFiche extends MY_Controller {
 
 	public function __construct() {
-		parent::__construct();		
+		parent::__construct();
 	}
 
 	public function index() {
-		$this->importerFiche();
+		$this -> importerFiche();
 	}
-	
+
 	public function importerFiche($data = array()) {
-		$this->load->library('layout');
-		$this->layout->views('menu_principal')
-					-> view('importer',array('error' => ' ' ));
+		$this -> load -> library('layout');
+		$this -> layout -> views('menu_principal') -> view('importer', array('error' => ' '));
 	}
-	
-	public function envoi(){
-		$config['upload_path'] = './assets/upload/';
-		$config['allowed_types'] = 'csv|tkt|xls|xml|jpg';
-		$config['max_size']	= '10000000';
-		$config['max_width']  = '102400000';
-		$config['max_height']  = '76800000';
-		
-		$this->load->library('upload', $config);
-	
-		if ( ! $this->upload->do_upload('fichier'))
-		{
-			$error = array('error' => $this->upload->display_errors());			
-			$this->load->library('layout');
-			$this->layout->views('menu_principal')
-					-> view('importer',$error);
-		}	
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());			
-			$this->load->view('upload_success', $data);
+
+	public function envoi() {
+		$nombreFichier = array('1', '2', '3', '4', '5', '6', '7');
+		$this -> load -> library('upload');
+
+		foreach ($nombreFichier as $i) {
+			if (!empty($_FILES['fichier_' . $i]['name'])) {
+				$config['file_name'] = $this -> session -> userdata('username') . '_fichier_' . $i;
+				$config['upload_path'] = './assets/upload';
+				$config['allowed_types'] = 'csv|xml|txt|xls|jpg';
+				$config['max_size'] = '2048';
+				$config['overwrite'] = TRUE;
+				$this -> upload -> initialize($config);
+				$form_name = 'fichier_' . $i;
+				if (!$this -> upload -> do_upload($form_name)) {
+					$error = array('error' => $this -> upload -> display_errors());
+					$this -> load -> library('layout');
+					$this -> layout -> views('menu_principal') -> view('importer', $error);
+				} else {
+					$data = array('upload_data' => $this -> upload -> data());
+					$this -> load -> library('layout');
+					$this -> layout -> views('menu_principal') -> view('importer');
+				}
+			}
 		}
 	}
+
 }
 ?>
--- beaubfm/application/views/importer.php
+++ beaubfm/application/views/importer.php
@@ -6,13 +6,13 @@
 	<div id="resultat_recherche">
 		<div id="cadre_importation">
 			<div class="cadre_selection">
-				<?php echo $error; ?>
+				<?php if(isset($error)) echo $error; ?>
 				<!-- Sélection de fichiers -->
 				<h1>Importation</h1>
 				<form  method="post" name="importer" id="importer" action="<?php echo site_url("importerFiche/envoi"); ?>" enctype="multipart/form-data">
 					<p>
-						<label class="lab_import" for="fichier">Fichier 1:</label>
-						<input size="60" type="file" name="fichier" id="fichier">
+						<label class="lab_import" for="fichier_1">Fichier 1:</label>
+						<input size="60" type="file" name="fichier_1" id="fichier_1">
 						<span id="leschamps_2"><a href="javascript:create_champ_fichier(2)"><i class="icon-plus-sign"></i></a></span>
 					</p>
 				</form>
