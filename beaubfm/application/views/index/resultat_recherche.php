dd2cf9c1ea46f16c34987d5242f3ef1bf140b8e6
MathieuCom <mathieu.c@sfr.fr>
2012-11-23 10:04:00 +0000

    Factorisation ENORME du code de la vue "resultat_recherche".

--- beaubfm/application/views/index/resultat_recherche.php
+++ beaubfm/application/views/index/resultat_recherche.php
@@ -1,41 +1,39 @@
-<?php 
-if($affichage != 0)
-{
-	if($affichage == 1 and isset($resultat))
-	{
-		?>
 <div id="cadre2">
 	<div id="selection">
 		<div class="infos-disque">
-		<ul class="icons" id="aff-disque">
-			<li>&nbsp;</li>
-			<li>&nbsp;</li>
-			<li id="chargement" class="chargement"><img src="<?php echo img_url('search.png'); ?>"> </li>
-			<li>&nbsp;</li>
-			<li>&nbsp;</li>
-		</ul>
+			<ul class="icons" <?php if(($affichage==1 && isset($resultat)) || $affichage==0) { ?> id="aff-disque" <?php } ?>>
+				<li>&nbsp;</li>
+				<li>&nbsp;</li>
+				<li id="chargement" class="chargement"><img src="<?php echo img_url('search.png'); ?>"> </li>
+				<li>&nbsp;</li>
+				<li>&nbsp;</li>
+			</ul>
 		</div>
 		<div id="cadre_action">
-			<!-- 	<a class="btn-large-action-nonActif"><i class="icon-pencil"></i> Modifier la fiche</a> -->
+			<a class="btn-large-action-nonActif"><i class="icon-pencil"></i> Modifier la fiche</a>
 			<a class="btn-large-action-nonActif"><i class="icon-trash"></i> Supprimer les fiches</a>
-			<a class="btn-large-action-nonActif" href="<?php echo base_url().'index.php/welcome/importer/' ;?>"><i class="icon-signin"></i> Importer des fiches</a>
 			<a class="btn-large-action-nonActif" href="#"><i class="icon-signout"></i> Exporter des fiches</a>
 		</div>
 	</div>
 	<div id="resultat_recherche">
+		<?php if($affichage==2){ ?>
+			<p>La recherche n'a renvoyé aucun résultat.</p>
+		<?php } ?>
+		<?php if($affichage!=0 && ($affichage!=1 || !isset($resultat)) && $affichage!=2){ ?>
+			<p>Veuillez saisir au moins une lettre pour faire une recherche.</p>
+		<?php } ?>
+		<?php if($affichage==0 || ($affichage==1 && isset($resultat))){ ?>		
 		<table>
 			<caption>
-				<?php echo count($resultat); ?> résultat(s) trouvé(s).
+				<?php if($affichage==0) { ?> Listes des disques <?php } ?>
+				<?php if($affichage==1) { echo count($resultat); ?> résultat(s) trouvé(s).<?php } ?>
 			</caption>
 			<tr>
-				<th>
-				<input type="checkbox" name="select" value="1">
-				</th>
+				<th><input type="checkbox" name="select" value="1"></th>
 				<th><i class="icon-music"></i> Titre</th>
 				<th><i class="icon-group"></i> Artiste</th>
 				<th><i class="icon-home"></i> Label</th>
 				<th class="tab-menu-action" colspan="2"><i class="icon-cogs"></i></th>
-
 			</tr>
 			<?php
 				$i=0;
@@ -55,123 +53,11 @@ if($affichage != 0)
 				}
 			?>
 		</table>
-		
-	</div>
-</div>
-<?php 	}
-	elseif($affichage==2) {
-			?>
-<div id="cadre2">
-	<div id="selection">
-		<div class="infos-disque">
-		<ul class="icons">
-			<li>&nbsp;</li>
-			<li>&nbsp;</li>
-			<li id="chargement" class="chargement"><img src="<?php echo img_url('search.png'); ?>"> </li>
-			<li>&nbsp;</li>
-			<li>&nbsp;</li>
-		</ul>
-		</div>
-		<div id="cadre_action">
-			<!-- 	<a class="btn-large-action-nonActif"><i class="icon-pencil"></i> Modifier la fiche</a> -->
-			<a class="btn-large-action-nonActif"><i class="icon-trash"></i> Supprimer les fiches</a>
-			<a class="btn-large-action-nonActif"><i class="icon-signin"></i> Importer des fiches</a>
-			<a class="btn-large-action-nonActif"><i class="icon-signout"></i> Exporter des fiches</a>
-		</div>
-	</div>
-	<div id="resultat_recherche">
-		<p>La recherche n'a renvoyé aucun résultat.</p>
-	</div>
-</div>
-
-			<?php
-		}else{
-			?>
-<div id="cadre2">
-	<div id="selection">
-		<div class="infos-disque">
-		<ul class="icons">
-			<li>&nbsp;</li>
-			<li>&nbsp;</li>
-						<li id="chargement" class="chargement"><img src="<?php echo img_url('search.png'); ?>"> </li>
-
-			<li>&nbsp;</li>
-			<li>&nbsp;</li>
-		</ul>
-		</div>
-		<div id="cadre_action">
-			<!-- 	<a class="btn-large-action-nonActif"><i class="icon-pencil"></i> Modifier la fiche</a> -->
-			<a class="btn-large-action-nonActif"><i class="icon-trash"></i> Supprimer les fiches</a>
-			<a class="btn-large-action-nonActif"><i class="icon-signin"></i> Importer des fiches</a>
-			<a class="btn-large-action-nonActif"><i class="icon-signout"></i> Exporter des fiches</a>
-		</div>
-	</div>
-	<div id="resultat_recherche">
-		<p>Veuillez saisir au moins une lettre pour faire une recherche.</p>
-	</div>
-</div>
-
-			<?php
-		}
-}
-else {
-	?>
-<div id="cadre2">
-	<div id="selection">
-		<div class="infos-disque">
-		<ul class="icons" id="aff-disque">
-			<li>&nbsp;</li>
-			<li>&nbsp;</li>
-			<li id="chargement" class="chargement"><img src="<?php echo img_url('search.png'); ?>"> </li>
-			<li>&nbsp;</li>
-			<li>&nbsp;</li>
-		</ul>
-		</div>
-		<div id="cadre_action">
-			<!-- 	<a class="btn-large-action-nonActif"><i class="icon-pencil"></i> Modifier la fiche</a> -->
-			<a class="btn-large-action-nonActif"><i class="icon-trash"></i> Supprimer les fiches</a>
-			<a class="btn-large-action-nonActif"><i class="icon-signin"></i> Importer des fiches</a>
-			<a class="btn-large-action-nonActif"><i class="icon-signout"></i> Exporter des fiches</a>
-		</div>
-	</div>
-	<div id="resultat_recherche">
-		<table>
-			<caption>
-				Listes des disques
-			</caption>
-			<tr>
-				<th>
-				<input type="checkbox" name="select" value="1">
-				</th>
-				<th><i class="icon-music"></i> Titre</th>
-				<th><i class="icon-group"></i> Artiste</th>
-				<th><i class="icon-home"></i> Label</th>
-				<th class="tab-menu-action" colspan="2"><i class="icon-cogs"></i></th>
-
-			</tr>
-			<?php
-				$i=0;
-				foreach ($resultat as $ligne) {
-					if ($i % 2 == 0)
-						echo '<tr class="blue">';
-					else
-						echo '<tr>';
-					echo '<td class="checkbox"><input type="checkbox" name="select" value="1"></td>';
-					echo '<td class="left" onclick="ajaxBox_loader(true);$.get(\''.site_url("index/affichage_disque/".$ligne['dis_id']).'\', function(data) { $(\'#aff-disque\').html(\'\').html(data); }).complete(function(){ajaxBox_loader(false);}).error(function(){ajaxBox_setText(\'Error...\');});">'.$ligne['dis_libelle'].'</td>';
-					echo '<td onclick="ajaxBox_loader(true);$.get(\''.site_url("index/affichage_disque/".$ligne['dis_id']).'\', function(data) { $(\'#aff-disque\').html(\'\').html(data); }).complete(function(){ajaxBox_loader(false);}).error(function(){ajaxBox_setText(\'Error...\');});">'.$ligne['art_nom'].'</td>';
-					echo '<td onclick="ajaxBox_loader(true);$.get(\''.site_url("index/affichage_disque/".$ligne['dis_id']).'\', function(data) { $(\'#aff-disque\').html(\'\').html(data); }).complete(function(){ajaxBox_loader(false);}).error(function(){ajaxBox_setText(\'Error...\');});">'.$ligne['per_nom'].'</td>';
-					echo '<td><a class="action-tab" href="#"><i class="icon-pencil"></a></td>';
-					echo '<td><a class="action-tab" href="#"><i class="icon-trash"></a></td>';
-					echo '</tr>';
-					$i++;
-				}
-			?>
-		</table>
-		<nav class="navig_result">
+		<?php if($affichage==0){ ?>
+			<nav class="navig_result">
 			<?php echo $pagination; ?>
-		</nav>
+			</nav>
+		<?php } ?>
+	<?php } ?>
 	</div>
 </div>
-
-	<?php
-}
