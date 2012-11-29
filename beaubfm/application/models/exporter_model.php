5f305827b24e906a07933a325acdaf4965023e66
LinkSC <h23j28@gmail.com>
2012-11-29 13:42:56 +0000

    Ajout exportation !!

---
+++ beaubfm/application/models/exporter_model.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Exporter_model extends CI_Model {
+	
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function select_export($where=array())
+	{
+			
+		if(empty($where)){
+			return $this->db->select('*')
+						->from('Disque')
+						->join('Artiste','Disque.per_id_artiste = Artiste.art_id')
+						->join('Label', 'Disque.dif_id = Label.lab_id')
+						->join('Utilisateur', 'Disque.uti_id_ecoute = Utilisateur.per_id')
+						->join('Emplacement', 'Disque.emp_id = Emplacement.emp_id')
+						->join('EmBenevole', 'Disque.emb_id = EmBenevole.emb_id','left')
+						->get();
+		}
+		elseif(!empty($where))
+		{
+				
+			return $this->db->select('*')
+							->from('Disque')
+							->join('Artiste','Disque.per_id_artiste = Artiste.art_id')
+							->join('Label', 'Disque.dif_id = Label.lab_id')
+							->join('Utilisateur', 'Disque.uti_id_ecoute = Utilisateur.per_id')
+							->join('Emplacement', 'Disque.emp_id = Emplacement.emp_id')
+							->join('EmBenevole', 'Disque.emb_id = EmBenevole.emb_id','left')
+							->where_in('dis_id', $where)
+							->get();
+		}
+	}
+}
