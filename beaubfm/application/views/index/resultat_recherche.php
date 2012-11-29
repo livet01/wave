198f15c9dfda6c3fbb0e746df7fb4fa42fb57a9d
LinkSC <h23j28@gmail.com>
2012-11-29 13:48:21 +0000

    Adaptation pour l'exporatation

--- beaubfm/application/views/index/resultat_recherche.php
+++ beaubfm/application/views/index/resultat_recherche.php
@@ -1,4 +1,4 @@
-
+<!-- <script src="<?php echo js_url('resultatRech'); ?>"></script> -->
 <div id="cadre2">
 	<div id="selection">
 		<div class="infos-disque">
@@ -13,7 +13,7 @@
 		<div id="cadre_action">
 			<a class="btn-large-action-nonActif"><i class="icon-pencil"></i> Modifier la fiche</a>
 			<a class="btn-large-action-nonActif"><i class="icon-trash"></i> Supprimer les fiches</a>
-			<a class="btn-large-action-nonActif" href="#"><i class="icon-signout"></i> Exporter des fiches</a>
+			<a id="exporter" class="btn-large-action-nonActif" href="#"><i class="icon-signout"></i> Exporter des fiches</a>
 		</div>
 	</div>
 	<div id="resultat_recherche">
@@ -24,26 +24,31 @@
 			<p>Veuillez saisir au moins une lettre pour faire une recherche.</p>
 		<?php } ?>
 		<?php if(($affichage==0 || $affichage==1) && !empty($resultat)){ ?>		
+		<div id="box"></div>
+		
 		<table>
 			<caption>
 				<?php if($affichage==0) { ?> Listes des disques <?php } ?>
 				<?php if($affichage==1) { echo count($resultat); ?> résultat(s) trouvé(s).<?php } ?>
 			</caption>
 			<tr>
-				<th><input type="checkbox" name="select" value="1"></th>
+				<th><input type="checkbox" onclick="CocheTout(this,'choix[]');" value="1" ></th>
 				<th><i class="icon-music"></i> Titre</th>
 				<th><i class="icon-group"></i> Artiste</th>
 				<th><i class="icon-home"></i> Label</th>
 				<th class="tab-menu-action" colspan="2"><i class="icon-cogs"></i></th>
 			</tr>
+			<tbody id="disque">
+			<form  method="post" id="tdisque" action="<?php echo site_url("exporterFiche"); ?>">
 			<?php
 				$i=0;
+				$j=0;
 				foreach ($resultat as $ligne) {
 					if ($i % 2 == 0)
 						echo '<tr class="blue">';
 					else
 						echo '<tr>';
-					echo '<td class="checkbox"><input type="checkbox" name="select" value="1"></td>';
+					echo '<td class="checkbox"><input class="chx" type="checkbox" name="choix[]" value="'.$ligne['dis_id'].'"></td>';
 					echo '<td class="left" onclick="ajaxBox_loader(true);$.get(\''.site_url("index/affichage_disque/".$ligne['dis_id']).'\', function(data) { $(\'#aff-disque\').html(\'\').html(data); }).complete(function(){ajaxBox_loader(false);}).error(function(){ajaxBox_setText(\'Error...\');});">'.$ligne['dis_libelle'].'</td>';
 					echo '<td onclick="ajaxBox_loader(true);$.get(\''.site_url("index/affichage_disque/".$ligne['dis_id']).'\', function(data) { $(\'#aff-disque\').html(\'\').html(data); }).complete(function(){ajaxBox_loader(false);}).error(function(){ajaxBox_setText(\'Error...\');});">'.$ligne['art_nom'].'</td>';
 					echo '<td onclick="ajaxBox_loader(true);$.get(\''.site_url("index/affichage_disque/".$ligne['dis_id']).'\', function(data) { $(\'#aff-disque\').html(\'\').html(data); }).complete(function(){ajaxBox_loader(false);}).error(function(){ajaxBox_setText(\'Error...\');});">'.$ligne['per_nom'].'</td>';
@@ -51,21 +56,24 @@
 					echo '<td><a class="action-tab" href="#"><i class="icon-trash"></a></td>';
 					echo '</tr>';
 					$i++;
+					$j++;
 				}
 			?>
+				</form>
+			</tbody>
 		</table>
-		<?php if($affichage==0){ ?>
+		<?php if(count($resultat)>15){ ?>
 			<nav class="navig_result">
-			<?php echo $pagination; ?>
+			<div class="holder"></div>
+			<!-- <?php echo $pagination; ?> -->
 			</nav>
-		<?php } ?>
 	<?php }
-
+	}
 		else {
-			?>
+		?>
 			<p>Bienvenue, il n'y a aucun disque dans la base de données. Pour commener veuillez ajouter un titre.</p>
-			<?php
+		<?php
 		}
-	?>
+		?>
 	</div>
 </div>
