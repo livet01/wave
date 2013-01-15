0a0015e26076f10f7c66053fb8d377c7c4080833
MathieuCom <mathieu.c@sfr.fr>
2013-01-15 13:42:47 +0000

    Allegement Bonfire 2eme partie :)

--- beaubfm/bonfire/application/core_modules/permissions/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['module_config'] = array(
-	'description'	=> 'Gérer les autorisations disponibles pour les rôles.',
-	'author'		=> 'Wave Team'
-);
